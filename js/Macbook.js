e21bf979f949ee05c883714a5a96ef7c08c093d6
Tonmoy-Muzumder <tonmoymuzumder1@gmail.com>
2021-08-21 12:33:29 +0600

    value-set

---
+++ js/Macbook.js
@@ -0,0 +1,42 @@
+// Extra Memory Cost
+document.getElementById('first-memory').addEventListener("click", function(){
+    const memoryCost = document.getElementById('memory-cost')
+    memoryCost.innerText = 0; 
+  
+});
+document.getElementById('second-memory').addEventListener("click", function(){
+    const memoryCost = document.getElementById('memory-cost')
+    memoryCost.innerText = 180; 
+  
+});
+// Extra Storage Cost
+document.getElementById('first-storage').addEventListener("click", function(){
+    const storageCost = document.getElementById('storage-cost')
+    storageCost.innerText = 0; 
+  
+});
+document.getElementById('second-storage').addEventListener("click", function(){
+    const storageCost = document.getElementById('storage-cost')
+    storageCost.innerText = 100; 
+  
+});
+document.getElementById('third-storage').addEventListener("click", function(){
+    const storageCost = document.getElementById('storage-cost')
+    storageCost.innerText = 180; 
+  
+});
+// delivery Charge
+document.getElementById('first-delivery').addEventListener("click", function(){
+    const deliveryCost = document.getElementById('delivery-cost')
+    deliveryCost.innerText = 0; 
+  
+});
+document.getElementById('second-delivery').addEventListener("click", function(){
+    const deliveryCost = document.getElementById('delivery-cost')
+    deliveryCost.innerText = 20; 
+  
+});
+
+// total cost
+
+
