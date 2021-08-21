9ee9046fe14646753e8162653f317a35f3c4e3f1
Tonmoy-Muzumder <tonmoymuzumder1@gmail.com>
2021-08-21 19:16:40 +0600

    javascript-added

--- js/Macbook.js
+++ js/Macbook.js
@@ -1,42 +1,93 @@
+// Variables Declaration
+const totalPrice = document.getElementById("total-price");
+const bestPrice = document.getElementById("best-price");
+let memoryCost = document.getElementById("memory-cost");
+const storageCost = document.getElementById("storage-cost");
+const deliveryCost = document.getElementById("delivery-cost");
+const totalCost = document.getElementById("total-cost");
+let newMemoryCost = 0,
+  newStorageCost = 0,
+  newDeliveryCost = 0;
+
+//Functions start
+
+const memory = (cost) => {
+  newMemoryCost = memoryCost.innerText = cost;
+  totalCost.innerHTML = 1299 + newMemoryCost;
+  return newMemoryCost;
+};
+
+const storage = (cost) => {
+  newStorageCost = storageCost.innerText = cost;
+  totalCost.innerHTML = 1299 + newStorageCost;
+  return newStorageCost;
+};
+
+const delivery = (cost) => {
+  newDeliveryCost = deliveryCost.innerText = cost;
+  return newDeliveryCost;
+};
+
+const total = () => {
+  totalCost.innerText = 1299 + newMemoryCost + newStorageCost + newDeliveryCost;
+};
+const finalTotal = () => {
+  totalPrice.innerText = 1299 + newMemoryCost + newStorageCost + newDeliveryCost;
+};
+//Functions end
+
+
 // Extra Memory Cost
-document.getElementById('first-memory').addEventListener("click", function(){
-    const memoryCost = document.getElementById('memory-cost')
-    memoryCost.innerText = 0; 
-  
+document.getElementById("first-memory").addEventListener("click", function () {
+  memory(0);
+  total();
+  finalTotal();
 });
-document.getElementById('second-memory').addEventListener("click", function(){
-    const memoryCost = document.getElementById('memory-cost')
-    memoryCost.innerText = 180; 
-  
+
+document.getElementById("second-memory").addEventListener("click", function () {
+  memory(180);
+  total();
+  finalTotal();
 });
+
 // Extra Storage Cost
-document.getElementById('first-storage').addEventListener("click", function(){
-    const storageCost = document.getElementById('storage-cost')
-    storageCost.innerText = 0; 
-  
+document.getElementById("first-storage").addEventListener("click", function () {
+  storage(0);
+  total();
+  finalTotal();
 });
-document.getElementById('second-storage').addEventListener("click", function(){
-    const storageCost = document.getElementById('storage-cost')
-    storageCost.innerText = 100; 
-  
+
+document.getElementById("second-storage").addEventListener("click", function () {
+    storage(100);
+    total();
+    finalTotal();
+  });
+document.getElementById("third-storage").addEventListener("click", function () {
+  storage(180);
+  total();
+  finalTotal();
 });
-document.getElementById('third-storage').addEventListener("click", function(){
-    const storageCost = document.getElementById('storage-cost')
-    storageCost.innerText = 180; 
-  
-});
+
 // delivery Charge
-document.getElementById('first-delivery').addEventListener("click", function(){
-    const deliveryCost = document.getElementById('delivery-cost')
-    deliveryCost.innerText = 0; 
-  
-});
-document.getElementById('second-delivery').addEventListener("click", function(){
-    const deliveryCost = document.getElementById('delivery-cost')
-    deliveryCost.innerText = 20; 
-  
-});
+document.getElementById("first-delivery").addEventListener("click", function () {
+    delivery(0);
+    total();
+    finalTotal();
+  });
+document.getElementById("second-delivery").addEventListener("click", function () {
+    delivery(20);
+    total();
+    finalTotal();
+  });
 
-// total cost
+// promo code and total
+    document.getElementById("promo-code-btn").addEventListener("click", function () {
+    const promoCode = document.getElementById("promo-code");
+    const inputField = promoCode.value;
 
-
+    if (inputField == "stevekaku") {
+      const discount = (20 * parseFloat(totalCost.innerHTML)) / 100;
+      totalPrice.innerHTML = parseFloat(totalCost.innerHTML) - discount;
+      promoCode.value = "";
+    }
+  });
